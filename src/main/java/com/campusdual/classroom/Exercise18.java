8b35e0ae754e4886bc3956143b383db339b059c5
Manuel Fernández Crego <mfernandezc22@fpcoruna.afundacion.org>
2024-09-30 21:42:51 +0200

    Ejer18 Completado

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -2,7 +2,26 @@ package com.campusdual.classroom;
 
 public class Exercise18 {
 
+	public static int[] createAndInitializeArray(int num) {
+		int[] Array = new int[num];
+		for (int i = 0; i < num; i++) {
+			Array[i] = i + 1;
+		}
+		return Array;
+	}
+
+	public static void showInlineArray(int[] Array) {
+		for (int i = 0; i < Array.length; i++) {
+			if (i != Array.length - 1) {
+				System.out.print(Array[i] + " ");
+			} else
+				System.out.print(Array[i]);
+		}
+	}
+
 	public static void main(String[] args) {
+		createAndInitializeArray(7);
+		showInlineArray(createAndInitializeArray(7));
 
 	}
 }
